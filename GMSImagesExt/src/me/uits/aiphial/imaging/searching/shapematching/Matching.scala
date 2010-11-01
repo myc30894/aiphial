033d8e9457038379c24813873006fb9ca9f15ab9
nickl <nickl@localhost>
2010-11-01 18:13:39 +0300

    Gary Baker's Hungarian method was removed. Now fully under LGPL.

--- GMSImagesExt/src/me/uits/aiphial/imaging/searching/shapematching/Matching.scala
+++ GMSImagesExt/src/me/uits/aiphial/imaging/searching/shapematching/Matching.scala
@@ -5,16 +5,16 @@
  * Copyright (c) 2010 Nicolay Mitropolsky <NicolayMitropolsky@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
@@ -70,53 +70,5 @@ object Matching {
     val minkey = map.keySet.min
     (minkey, map.get(minkey).get)
   }
-
-  /**
- * matches two shape contexts with shapes hungarian method.
- * @return tuple contains distance value and list of matched points pairs
- */
-  def hungarianMatch(a: ShapeContext, b: ShapeContext): (Double, List[(LuvPoint, LuvPoint)]) = {
-
-
-    val arr1:Array[PointContext] = a.pointContexts.toArray[PointContext](Array.ofDim[PointContext](a.pointContexts.size))
-    val arr2:Array[PointContext] = b.pointContexts.toArray[PointContext](Array.ofDim[PointContext](b.pointContexts.size))
-
-    val costMatrix = Array.ofDim[Float](arr1.length, arr2.length)
-
-    for ((ai, i) <- arr1.zipWithIndex) {
-      for ((bj, j) <- arr2.zipWithIndex) {
-         costMatrix(i)(j) = (ai euclideanReduce bj).toFloat
-      }
-    }
-
-    import assignment._
-
-    val ap = new AssignmentProblem(costMatrix).solve(new HungarianAlgorithm)
-
-//   for(line <- ap){
-//     println(line.mkString("[",",","]"))
-//   }
-//
-
-
-   val ab = new ArrayBuffer[(LuvPoint, LuvPoint)](arr1.size)
-   var sum = 0f
-   for(line <- ap){
-
-     val e1 = line(0)
-     val e2 = line(1)
-
-     ab+=((arr1(e1).point,arr2(e2).point))
-     sum+=costMatrix(e1)(e2)
-   }
-
-    //todo complete it
-
-    return (sum,ab.toList)
-  }
-
-
-
-
 
 }
