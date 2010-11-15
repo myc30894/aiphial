f751ec807d256d89e03a82073d1dd219f13ef902
nickl <nickl@localhost>
2010-11-15 17:00:05 +0300

    missing copyrights were added. imageScaler was dropped

--- GMSImages/src/me/uits/aiphial/imaging/Matrix.scala
+++ GMSImages/src/me/uits/aiphial/imaging/Matrix.scala
@@ -1,12 +1,32 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
+ * This file is part of Aiphial.
+ *
+ * Copyright (c) 2010 Nicolay Mitropolsky <NicolayMitropolsky@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package me.uits.aiphial.imaging
 
 import java.util.Arrays
 
+/**
+ *
+ * @author Nicolay Mitropolsky <NicolayMitropolsky@gmail.com>
+ */
 class Matrix[T] private (private val data:Array[Array[T]])(implicit Tmf:ClassManifest[T]) {
 
   val height = data.length
--- GeneralMeanShift/src/me/uits/aiphial/general/basic/ProgressListener.java
+++ GeneralMeanShift/src/me/uits/aiphial/general/basic/ProgressListener.java
@@ -1,16 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
+ * This file is part of Aiphial.
+ *
+ * Copyright (c) 2010 Nicolay Mitropolsky <NicolayMitropolsky@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package me.uits.aiphial.general.basic;
 
 /**
- *
- * @author nickl
+ * Interface for progress listeners, that could be attached to
+ * a long-lasting procedure and then notified how much is done
+ * @author Nicolay Mitropolsky <NicolayMitropolsky@gmail.com>
  */
 public interface ProgressListener {
 
+    /**
+     * Is called when some work is done
+     * @param val [0..1] what part of work is done
+     */
     void onStepDone(float val);
 
 }
